f709a6f4533d68b27086f7a9e201b7e607bd842f
Dev-efil <thanushkanth95@gmail.com>
2019-09-06 23:13:39 +0530

    build(core): implement onSubmit

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,14 +8,26 @@ import { ContactMessage } from '../model/contact-message.model';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  contactModel = new ContactMessage('','a@.COM','sub','hi hello');
+  contactModel = new ContactMessage('','','','');
+  submitted = false;
 
-  constructor(private _service:ContactMessageService) 
-  {
-      
-  }
+  constructor(private _messageService:ContactMessageService) {}
 
   ngOnInit() {
   }
 
+  onSubmit()
+  {
+    this.submitted = true;
+    this._messageService.sendMessage(this.contactModel)
+    .subscribe(
+      data => console.log('Success!', data),
+      error => console.error('Error!', error)
+    )
+    
+  }
+  // clearForm()
+  // {
+  //   this.form.resetForm(this.contactModel);
+  // }
 }
